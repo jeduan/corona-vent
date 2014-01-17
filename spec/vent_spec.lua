d5e9ca5264cc8b4305d2581fd32e7c9f294bd538
Jeduan Cornejo <jeduan@gmail.com>
2014-01-17 14:56:24 -0600

    fixed spec

--- spec/vent_spec.lua
+++ spec/vent_spec.lua
@@ -14,7 +14,6 @@ describe("Testing vent", function()
 	it("calls a listener", function(done)
 		eventually(async(function()
 			local function listener()
-				print'hola'
 				done()
 			end
 			vent:on('test1', listener)
@@ -23,41 +22,59 @@ describe("Testing vent", function()
 	end)
 
 	it("calls a listener with data", function(done)
-		local function listener(event)
-			assert.is_true(event.ok)
-			done()
-		end
-		vent:on('test', listener)
-		vent:trigger('test', {ok = true})
+		eventually(async(function()
+			local function listener(event)
+				assert.is_true(event.ok)
+				done()
+			end
+			vent:on('test2', listener)
+			vent:trigger('test2', {ok = true})
+		end))
 	end)
 
 	it('doesnt call a function after off', function(done)
-		local function listener()
-			assert.True(false, 'function was called')
-		end
-		vent:on('test', listener)
-		vent:off('test', listener)
+		eventually(async(function()
+			local function listener()
+				assert.True(false, 'function was called')
+			end
+			vent:on('test3', listener)
+			vent:off('test3', listener)
+			vent:trigger('test')
+			timer.performWithDelay(0.2, function()
+				done()
+			end)
+		end))
 	end)
 
 	it('doesnt call a function after allOff', function(done)
-		local function listener()
-			assert.True(false, 'function was called')
-		end
-		vent:on('test', listener)
-		vent:allOff('test')
+		eventually(async(function()
+			local function listener()
+				assert.True(false, 'function was called')
+			end
+			vent:on('test4', listener)
+			vent:allOff('test4')
+			vent:trigger('test4')
+			timer.performWithDelay(0.2, function()
+				done()
+			end)
+		end))
 	end)
 
 	it("calls a listener once", function(done)
-		local timescalled = 0
-		local function listener(event)
-			print 'on listener'
-			timescalled = timescalled + 1
-			assert.True(timescalled == 1, 'Function was called multiple times')
-		end
+		eventually(async(function()
+			local timescalled = 0
+			local function listener(event)
+				timescalled = timescalled + 1
+				assert.True(timescalled == 1, 'Function was called multiple times')
+			end
 
-		vent:once('test', listener)
-		vent:trigger('test')
-		vent:trigger('test')
+			vent:once('test5', listener)
+			vent:trigger('test5')
+			vent:trigger('test5')
+			timer.performWithDelay(0.2, function()
+				done()
+			end)
+		end))
 	end)
 
 end)
